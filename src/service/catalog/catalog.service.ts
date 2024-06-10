25c95882fdc223a5ebaf777fcc17698e732bbe0e
nyantsaranja <99872725+nyantsaranja@users.noreply.github.com>
2024-06-10 09:29:06 +0300

    css looks

--- src/service/catalog/catalog.service.ts
+++ src/service/catalog/catalog.service.ts
@@ -9,14 +9,14 @@ export class CatalogService {
 
   getCatalogItems() {
     return [
-      { name: "Aragorn's Sword", price: 500, image: 'assets/aragorn-sword.jpg' },
-      { name: "Gandalf's Staff", price: 300, image: 'assets/gandalf-staff.jpg' },
-      { name: "Frodo's Mithril Shirt", price: 350, image: 'assets/frodo-mithril-shirt.jpg' },
-      { name: "Galadriel's Mirror", price: 200, image: 'assets/galadriel-mirror.jpg' },
-      { name: "Sauron's Mace", price: 400, image: 'assets/sauron-mace.jpg' },
-      { name: "Eowyn's Shieldmaiden Armor", price: 450, image: 'assets/eowyn-armor.jpg' },
-      { name: "Legolas' Bow and Arrows", price: 250, image: 'assets/legolas-bow.jpg' },
-      { name: "Bilbo's Magic Ring", price: 150, image: 'assets/bilbo-ring.jpg' }
+      { name: "Baton enchanté de Lumos", price: 500, image: 'assets/products/Enchanted-wand-of-lumos.webp' },
+      { name: "Potion d'éternel jeunesse", price: 300, image: 'assets/products/Potion-of-eternal-youth.webp' },
+      { name: "Crystal mythique", price: 350, image: 'assets/products/Mystic-crystal-orb.webp' },
+      { name: "Elixir d'invisibilité", price: 200, image: 'assets/products/Elixir-of-invisibility.webp' },
+      { name: "Dictionnaire des sorciers", price: 400, image: 'assets/products/Sorcerers-spellbook.webp' },
+      { name: "Amulette de protection", price: 450, image: 'assets/products/Amulet-of-protection.webp' },
+      { name: "Balaie de sorcier", price: 250, image: 'assets/products/Witchs-broomstick.webp' },
+      { name: "Plume du phoenix", price: 150, image: 'assets/products/Phoenix-feather-quill.webp' }
     ];
   }
 }
